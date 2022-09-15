e6545aa4179976af9c49d19812810aed5bfb8457
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-09-15 13:25:33 +0000

    added setState to change components, added a callback to console log the correct value rather than old value

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
-      <Sidebar />
+      <StatefulGreeting greeting="I'm a stateful class component" name="Antonio"/>
     </div>
   );
 }
