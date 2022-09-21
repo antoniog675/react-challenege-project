12ee19c38e2f48fff436f6474297912edcac58df
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-09-21 08:33:49 +0000

    changing parent props from the child

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import css from './App.module.css';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 import NestingComponents from './components/NestingComponents';
 
 function App() {
   return (
     <div className={css.App}>
-      <NestingComponents />
+      <MethodsAsPropsParent />
     </div>
   );
 }
