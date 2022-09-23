088f16e81b634fa67895962b5d3c0083f8358d50
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-09-23 12:36:45 +0000

    created and practiced component DidMount, used child to render

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import css from './App.module.css';
-import Content from './components/Content';
+import LifeCyclesCDM from './components/LifeCyclesCDM';
 
 function App() {
   return (
     <div className={css.App}>
-      <Content />
+      <LifeCyclesCDM />
     </div>
   );
 }
