810824c0ddac50ab50af45b8680a558e8b8e2999
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-09-26 09:08:14 +0000

    useed useState to update objects

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import css from './App.module.css';
 // import SearchBar from './components/SearchBar';
 // import Content from './components/Content';
-import UseStateWithArrays from './components/UseStateWithArrays';
+// import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 function App() {
   return (
     <div className={css.App}>
       {/* <SearchBar /> */}
       {/* <Content /> */}
-      <UseStateWithArrays />
+      {/* <UseStateWithArrays /> */}
+      <UseStateWithObjects />
     </div>
   );
 }
